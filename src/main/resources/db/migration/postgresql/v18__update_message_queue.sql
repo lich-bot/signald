0bd48c87c83998299a5ade4fa1e7f5b241df2f85
finn <finn@janky.solutions>
2022-12-21 21:47:01 -0800

    fix server params

--- src/main/resources/db/migration/postgresql/v18__update_message_queue.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE signald_message_queue DROP COLUMN legacy_message;
-ALTER TABLE signald_message_queue ADD COLUMN urgent BOOL DEFAULT FALSE;
-ALTER TABLE signald_message_queue ADD COLUMN updated_pni TEXT;
-ALTER TABLE signald_message_queue ADD COLUMN story BOOL DEFAULT FALSE;
--- src/main/resources/db/migration/sqlite/V18__update_message_queue.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE message_queue DROP COLUMN legacy_message;
-ALTER TABLE message_queue ADD COLUMN urgent BOOL DEFAULT FALSE;
-ALTER TABLE message_queue ADD COLUMN updated_pni TEXT;
-ALTER TABLE message_queue ADD COLUMN story BOOL DEFAULT FALSE;
